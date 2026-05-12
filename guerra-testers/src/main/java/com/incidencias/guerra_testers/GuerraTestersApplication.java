faa57848268dee17a45cbf898d849c7631f62ebe
Angel <davidasdadad@gmail.com>
2026-05-12 15:29:42 -0500

    Creación proyecto Spring Boot

---
+++ guerra-testers/src/main/java/com/incidencias/guerra_testers/GuerraTestersApplication.java
@@ -0,0 +1,13 @@
+package com.incidencias.guerra_testers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuerraTestersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuerraTestersApplication.class, args);
+	}
+
+}
